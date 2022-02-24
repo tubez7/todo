b92fe208a9d477c065bb14524f4e67719c26c4bc
tubez7 <thescopeboys@hotmail.com>
2022-02-24 23:44:53 +0000

    add comments and update some variable names

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
 import "./App.css";
 import { useState } from "react";
 
-function App() {
-  const [toDos, setToDos] = useState([
+function App() { //return value injects into the <div id="root"></div> on index.html
+  const [toDos, setToDos] = useState([ //mounted state of toDos array
     "build a to do list",
     "get confused",
     "push to git",
   ]);
 
+  console.log(toDos); //confirms toDos array has been updated
+
   return (
     <div className="App">
-      <h1>Richard and Harrison's Todo List </h1>
+      <h1>Richard and Harrison's To-Do List </h1>  {/* could be extracted into its own component */}
 
-      <fieldset>
+      <fieldset> {/* could be extracted into its own component */}
         <ul>
           {toDos.map((toDo, index) => {
             return (
-              <li key={`${toDo}-${index}`} className="card">
+              <li key={`${toDo}-${index}`} className="card"> {/* use index to assign a unique key as no id available */}
                 <p>{toDo}</p>
               </li>
             );
           })}
         </ul>
       </fieldset>
-      <input id="add-todo" placeholder="Enter a todo here"></input>
+      <input id="add-todo" placeholder="Enter a to-do here"></input>
       <button
         onClick={() => {
-          console.log("clicked");
-          setToDos((junk) => {
-            const newToDos = [...junk];
+          console.log("clicked"); //confirms successful click
+          setToDos((currentToDos) => { //setToDos function sets the new state of toDos array
+            const newToDos = [...currentToDos]; //must not mutate the current state so spread value into new state variable.
             newToDos.push("hello");
-            return newToDos;
+            return newToDos; //return value of setToDos = new state of toDos
           });
         }}
       >
         Add Todo
-      </button>
+      </button> {/* button component could be extracted */}
     </div>
   );
 }
