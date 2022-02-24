3a024a7612e568e680803d3b14f57795892c5ff2
tubez7 <thescopeboys@hotmail.com>
2022-02-24 16:52:35 +0000

    add items to do list on press of button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [toDos, setToDos] = useState([
+    "build a to do list",
+    "get confused",
+    "push to git",
+  ]);
+
+  return (
+    <div className="App">
+      <h1>Richard and Harrison's Todo List </h1>
+
+      <fieldset>
+        <ul>
+          {toDos.map((toDo, index) => {
+            return (
+              <li key={`${toDo}-${index}`} className="card">
+                <p>{toDo}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </fieldset>
+      <input id="add-todo" placeholder="Enter a todo here"></input>
+      <button
+        onClick={() => {
+          console.log("clicked");
+          setToDos((junk) => {
+            const newToDos = [...junk];
+            newToDos.push("hello");
+            return newToDos;
+          });
+        }}
+      >
+        Add Todo
+      </button>
+    </div>
+  );
 }
 
 export default App;
